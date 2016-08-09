1a3498196f8eba1d4f358407517f330fcc746efe
Dana Ottaviani <dana.ottaviani@gmail.com>
2016-08-09 14:44:08 -0400

    added hackers.insert

--- client/newHackerForm/newHackerForm.js
+++ client/newHackerForm/newHackerForm.js
@@ -15,7 +15,7 @@ Template.newHackerForm.events({
       allegiance: allegiance
     }
 
-    Hackers.insert(newHacker);
+    Meteor.call('hackers.insert',{newHackerObject:newHacker});
     alert("successful");
   }
 
